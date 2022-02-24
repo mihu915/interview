893573f36ad8cfd08a53e4ad16555698f6d733e3
misted <mihu0915@163.com>
2022-02-24 14:47:38 +0800

    面试题

--- 01-递归随机数数组/index.js
+++ 01-递归随机数数组/index.js
@@ -1,5 +1,16 @@
 const arr = new Array(5)
+let index = 0
+const handleSetArray = () => {
+  const rand = Math.floor(Math.random() * 31 + 2)
+  const isExist = arr.find(item => {
+    return item === rand
+  })
+  if (!isExist) {
+    arr[index] = rand
+    index++
+    if (index === arr.length) return
+  }
+  handleSetArray()
+}
+handleSetArray()
 console.log(arr)
-
-console.log(Math.floor(Math.random() * 31 + 2))
-
